82cf85f0099427a0899ef190a6a35f30b1176b37
Pablo Gonzalez <pabgon0@gmail.com>
2020-08-14 20:15:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,5 +9,7 @@ function handleClick(e) {
 
 /* End Given Code, don't edit above here...*/
 
-/* Write your implementation of greet() */
+function greet(string) {
+  
+}
 /* Write your implementation of displayMessage() */
